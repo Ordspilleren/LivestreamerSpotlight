05dfd49af4afa44dd67745a78a794244bd52ce2d
Lasse Bjerre <lasse@lgb.dk>
2015-05-19 21:18:53 +0200

    Added basic functionality

--- LivestreamerSpotlight/MainWindow.xaml.cs
+++ LivestreamerSpotlight/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace LivestreamerSpotlight
 {
@@ -25,8 +26,6 @@ namespace LivestreamerSpotlight
             InitializeComponent();
 
             MouseLeftButtonDown += (o, e) => DragMove();
-            this.PreviewKeyDown += new KeyEventHandler(HandleEsc);
-
         }
 
         private void HandleEsc(object sender, KeyEventArgs e)
@@ -34,5 +33,26 @@ namespace LivestreamerSpotlight
             if (e.Key == Key.Escape)
                 Close();
         }
+
+        void EnterPressed(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                Process startStream = new Process();
+                startStream.StartInfo.FileName = Environment.CurrentDirectory + "/livestreamer/livestreamer.exe";
+                startStream.StartInfo.Arguments = @"--player ""C:\Program Files\MPC-HC\mpc-hc64.exe"" --player-passthrough=http,hls,rtmp twitch.tv/" + streamName.Text + " best";
+                startStream.StartInfo.CreateNoWindow = true;
+                startStream.StartInfo.UseShellExecute = false;
+                startStream.Start();
+
+                Process startChat = new Process();
+                startChat.StartInfo.FileName = "chrome";
+                startChat.StartInfo.Arguments = "--app=http://www.twitch.tv/chat/embed?channel=" + streamName.Text + "&popout_chat=true";
+                startChat.Start();
+
+                Close();
+            }
+        }
+
     }
 }
--- LivestreamerSpotlight/Properties/Settings.Designer.cs
+++ LivestreamerSpotlight/Properties/Settings.Designer.cs
@@ -8,22 +8,66 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace LivestreamerSpotlight.Properties
-{
-
-
+namespace LivestreamerSpotlight.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string player {
+            get {
+                return ((string)(this["player"]));
+            }
+            set {
+                this["player"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("twitch")]
+        public string service {
+            get {
+                return ((string)(this["service"]));
+            }
+            set {
+                this["service"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("best")]
+        public string quality {
+            get {
+                return ((string)(this["quality"]));
+            }
+            set {
+                this["quality"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool chat {
+            get {
+                return ((bool)(this["chat"]));
+            }
+            set {
+                this["chat"] = value;
             }
         }
     }
