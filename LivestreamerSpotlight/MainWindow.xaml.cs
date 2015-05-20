b016b3b009d0f2767df57a98f6e7f335a99088d8
Lasse Bjerre <lasse@lgb.dk>
2015-05-20 12:17:14 +0200

    Removed settings dialog for now and reverted to simple settings file

--- LivestreamerSpotlight/MainWindow.xaml.cs
+++ LivestreamerSpotlight/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Diagnostics;
+using System.IO;
 
 namespace LivestreamerSpotlight
 {
@@ -30,22 +31,28 @@ namespace LivestreamerSpotlight
 
         private void HandleEsc(object sender, KeyEventArgs e)
         {
-            SettingsWindow settings = new SettingsWindow();
+            //SettingsWindow settings = new SettingsWindow();
 
             if (e.Key == Key.Escape)
                 Application.Current.Shutdown();
 
+            /*
             if (e.Key == Key.F1)
                 settings.Show();
+                */
         }
 
         void EnterPressed(object sender, KeyEventArgs e)
         {
+            var config = File.ReadAllLines(Environment.CurrentDirectory + "/settings.cfg")
+              .Select(l => l.Split(new[] { '=' }))
+              .ToDictionary(s => s[0].Trim(), s => s[1].Trim());
+
             if (e.Key == Key.Return)
             {
                 string service = "twitch.tv";
 
-                switch (Properties.Settings.Default.service)
+                switch (config["service"])
                 {
                     case "twitch":
                         service = "twitch.tv/";
@@ -57,12 +64,12 @@ namespace LivestreamerSpotlight
 
                 Process startStream = new Process();
                 startStream.StartInfo.FileName = Environment.CurrentDirectory + "/livestreamer/livestreamer.exe";
-                startStream.StartInfo.Arguments = @"--player " + '"' + Properties.Settings.Default.player + '"' + " --player-passthrough=http,hls,rtmp " + service + streamName.Text + " " + Properties.Settings.Default.quality;
+                startStream.StartInfo.Arguments = @"--player " + '"' + config["player"] + '"' + " --player-passthrough=http,hls,rtmp " + service + streamName.Text + " " + config["quality"];
                 startStream.StartInfo.CreateNoWindow = true;
                 startStream.StartInfo.UseShellExecute = false;
                 startStream.Start();
 
-                if (Properties.Settings.Default.chat == true)
+                if (config["chat"] == "yes")
                 {
                     Process startChat = new Process();
                     startChat.StartInfo.FileName = "chrome";
