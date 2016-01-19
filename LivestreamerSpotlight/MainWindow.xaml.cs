3d8dc0d5878272d8e4391ed5d810005c0b6b70f2
Lasse Bjerre <lasse@lgb.dk>
2016-01-19 15:57:06 +0100

    Removed hotkey handling and added quality selection

--- LivestreamerSpotlight/MainWindow.xaml.cs
+++ LivestreamerSpotlight/MainWindow.xaml.cs
@@ -26,62 +26,37 @@ namespace LivestreamerSpotlight
         {
             InitializeComponent();
 
-            this.Visibility = Visibility.Hidden;
+            //this.Visibility = Visibility.Hidden;
             MouseLeftButtonDown += (o, e) => DragMove();
-            HotKey _hotKey = new HotKey(Key.S, KeyModifier.Shift | KeyModifier.Win, OnHotKeyHandler);
-        }
-
-        private async void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            Debug.WriteLine(await Services.TwitchTV("Aui_2000", "low"));
+            //HotKey _hotKey = new HotKey(Key.S, KeyModifier.Shift | KeyModifier.Win, OnHotKeyHandler);
         }
 
         private void OnHotKeyHandler(HotKey hotKey)
         {
-            this.Visibility = Visibility.Visible;
+            //this.Visibility = Visibility.Visible;
         }
 
         private void HandleEsc(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Escape)
-                this.Visibility = Visibility.Hidden;
+                //this.Visibility = Visibility.Hidden;
+                Application.Current.Shutdown();
         }
 
-        void EnterPressed(object sender, KeyEventArgs e)
+        async void EnterPressed(object sender, KeyEventArgs e)
         {
-            var config = File.ReadAllLines(Environment.CurrentDirectory + "/settings.cfg")
-                .Select(l => l.Split(new[] { '=' }))
-                .ToDictionary(s => s[0].Trim(), s => s[1].Trim());
-
             if (e.Key == Key.Return)
             {
-                string service = "twitch.tv";
-
-                switch (config["service"])
-                {
-                    case "twitch":
-                        service = "twitch.tv/";
-                        break;
-                    case "hitbox":
-                        service = "hitbox.tv/";
-                        break;
-                }
-
+                string[] args = Environment.GetCommandLineArgs();
                 Process startStream = new Process();
-                startStream.StartInfo.FileName = "livestreamer";
-                startStream.StartInfo.Arguments = @"--player " + '"' + config["player"] + '"' + " --player-passthrough=http,hls,rtmp " + service + streamName.Text + " " + config["quality"];
+                startStream.StartInfo.FileName = args[2];
+                startStream.StartInfo.Arguments = await Services.TwitchTV(streamName.Text, streamQuality.Text);
                 startStream.StartInfo.CreateNoWindow = true;
                 startStream.StartInfo.UseShellExecute = false;
                 startStream.Start();
 
-                if (config["chat"] == "yes")
-                {
-                    Process startChat = new Process();
-                    startChat.StartInfo.FileName = "chrome";
-                    startChat.StartInfo.Arguments = "--app=http://www.twitch.tv/chat/embed?channel=" + streamName.Text + "&popout_chat=true";
-                    startChat.Start();
-                }
-                this.Visibility = Visibility.Hidden;
+                //this.Visibility = Visibility.Hidden;
+                Application.Current.Shutdown();
             }
         }
 
